d827fb56b2d0ad0e274cf5f53ee1a7204b60a7b1
Yichen Huang <yichen0831@gmail.com>
2016-01-18 15:47:07 +0800

    Added SymmetricMatrix in triangle_matrix.py

--- matrix/triangle_matrix.py
+++ matrix/triangle_matrix.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class UpperTriangleMatrix:
+class TriangleMatrix:
+    """
+    triangle matrix/array of m*m elements
+    """
+    def __init__(self, array):
+        self._m = len(array)
+        self._array = []
+
+    def get_original_matrix(self):
+        return []
+
+    def __str__(self):
+        original_matrix = self.get_original_matrix()
+        return '\n'.join(map(str, original_matrix))
+
+
+class UpperTriangleMatrix(TriangleMatrix):
     def __init__(self, array):
         """
         array is a m*m upper triangle array/matrix
         """
-        self._m = len(array)
-        self._array = []
+        super(UpperTriangleMatrix, self).__init__(array)
         for row in array:
             for v in row:
                 if v != 0:
@@ -21,23 +36,18 @@ class UpperTriangleMatrix:
                 if i > j:
                     row.append(0)
                 else:
-                    v = self._array[self._m * i - (i+1) * i//2 + (j)]
+                    v = self._array[self._m * i - (i+1) * i//2 + j]
                     row.append(v)
             original_matrix.append(row)
         return original_matrix
 
-    def __str__(self):
-        original_matrix = self.get_original_matrix()
-        return '\n'.join(map(str, original_matrix))
 
-
-class LowerTriangleMatrix:
+class LowerTriangleMatrix(TriangleMatrix):
     def __init__(self, array):
         """
         array is a m*m lower triangle array/matrix
         """
-        self._m = len(array)
-        self._array = []
+        super(LowerTriangleMatrix, self).__init__(array)
         for row in array:
             for v in row:
                 if v != 0:
@@ -56,9 +66,28 @@ class LowerTriangleMatrix:
             original_matrix.append(row)
         return original_matrix
 
-    def __str__(self):
-        original_matrix = self.get_original_matrix()
-        return '\n'.join(map(str, original_matrix))
+
+class SymmetricMatrix(TriangleMatrix):
+    def __init__(self, array):
+        super(SymmetricMatrix, self).__init__(array)
+        for i in range(self._m):
+            for j in range(self._m):
+                if i > j:
+                    continue
+                self._array.append(array[i][j])
+
+    def get_original_matrix(self):
+        original_matrix = []
+        for i in range(self._m):
+            row = []
+            for j in range(self._m):
+                if i > j:
+                    v = self._array[self._m * j - (j+1) * j//2 + i]
+                else:
+                    v = self._array[self._m * i - (i+1) * i//2 + j]
+                row.append(v)
+            original_matrix.append(row)
+        return original_matrix
 
 
 if __name__ == '__main__':
@@ -78,6 +107,14 @@ if __name__ == '__main__':
         [11, 12, 13, 14, 15]
     ]
 
+    symmetric_array = [
+        [1, 2, 3, 4, 5],
+        [2, 6, 7, 8, 9],
+        [3, 7, 10, 11, 12],
+        [4, 8, 11, 13, 14],
+        [5, 9, 12, 14, 15]
+    ]
+
     upper_triangle_matrix = UpperTriangleMatrix(upper_array)
     print(upper_triangle_matrix)
 
@@ -85,3 +122,8 @@ if __name__ == '__main__':
 
     lower_triangle_matrix = LowerTriangleMatrix(lower_array)
     print(lower_triangle_matrix)
+
+    print('* ' * 8)
+
+    symmetric_matrix = SymmetricMatrix(symmetric_array)
+    print(symmetric_matrix)
